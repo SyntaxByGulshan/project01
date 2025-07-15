2cd941cad082da20aafb95086e4838fcc0e4499e
Gulshan kumar <gul19june@gmail.com>
2025-07-15 17:06:39 +0530

    update02

--- header.js
+++ header.js
@@ -1,3 +1,19 @@
+function show(){
 console.log("hello world");
-const header=document.querySelector('header')
-console.log(header)
+
+const opt=document.querySelector('.options')
+const arr1=document.querySelector('.vector1')
+const arr2=document.querySelector('.vector2')
+console.log(opt)
+if(opt.classList.contains('hidden')){
+opt.classList.remove('hidden')
+opt.classList.add('flex');
+arr1.classList.add('hidden')
+arr2.classList.remove('hidden')
+}else{
+   opt.classList.remove('flex')
+   opt.classList.add('hidden'); 
+   arr2.classList.add('hidden')
+   arr1.classList.remove('hidden')
+}
+}
